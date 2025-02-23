c6d8297e563552a7b0c8c7f385237c51baadef06
AnjaliPatel2003 <anjalikiranpatel@gmail.com>
2025-02-23 15:02:30 +0530

    Update final.py

--- final.py
+++ final.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib
+
+import matplotlib as mp
 import matplotlib.pyplot as plt
 import seaborn as sns
 from statsmodels.tsa.stattools import adfuller, kpss
